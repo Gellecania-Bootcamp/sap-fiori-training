1dc7e3957ade0ea3336b7b30fddbcc589f4074c9
Emika Gellecania <e.gellecania@accenture.com>
2025-05-24 09:59:36 +0000

    functional clear button

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -5,6 +5,17 @@ sap.ui.define([
 
     return Controller.extend("com.acn.training.project1.controller.View1", {
         onInit() {
+        },
+
+        onClearPress() {
+            const oView = this.getView();
+            //Clear input fields
+            oView.byId("input4").setValue("");
+            oView.byId("input7").setValue("");
+            oView.byId("input8").setValue("");
+            oView.byId("input10").setValue("");
+            oView.byId("input11").setValue("");
+            oView.byId("select0").setSelectedKey("");
         }
     });
 });
